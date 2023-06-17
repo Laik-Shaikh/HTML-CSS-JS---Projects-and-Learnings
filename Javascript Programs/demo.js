eeebb764a9f7bec2bb79440bacf2ef3074dd97fd
Laik Shaikh <rengoku1502@gmail.com>
2023-06-17 22:34:07 +0530

    HTML CSS JS Projects and Learnings

---
+++ Javascript Programs/demo.js
@@ -0,0 +1,7 @@
+const date = new Date();
+
+const currentDate = date.getDate();
+const currentMonth = date.getMonth() + 1;
+const currentYear = date.getFullYear();
+
+console.log(currentDate + "-" + currentMonth + "-" + currentYear);
